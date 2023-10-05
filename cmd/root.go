5cf6b5de1e785de3b6e17fdca941eae466c7fe4d
orangekame3 <miya.org.0309@gmail.com>
2023-10-05 17:46:41 +0900

    update

--- cmd/root.go
+++ cmd/root.go
@@ -29,27 +29,33 @@ import (
 	"strings"
 	"time"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/dmowcomber/go-clear"
 	"github.com/spf13/cobra"
 )
 
+var color string
+var speed int32
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "paclear",
-	Short: "A brief description of your application",
+	Short: "paclear is a clear command with pacman animation",
 	Run:   paclear,
 }
 
 func paclear(cmd *cobra.Command, args []string) {
+	styledPac := style(color, pac)
 	rows, cols := getSize()
 	width := len(pac[0])
 	height := len(pac)
+	pitch := time.Duration(10 / time.Duration(speed) * time.Millisecond)
 	for y := 0; y <= rows-height; y += height {
 		for x := 0; x <= cols-width/3; x++ {
-			for j, line := range pac {
+			for j, line := range styledPac {
 				fmt.Printf("\033[%d;%dH%s", y+j+1, x, line)
 			}
-			time.Sleep(10 * time.Millisecond)
+			time.Sleep(pitch)
 			for k := 0; k < height; k++ {
 				fmt.Printf("\033[%d;%dH%s", y+k+1, x, strings.Repeat(" ", width))
 			}
@@ -66,7 +72,8 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringVarP(&color, "color", "c", "default", "Set pacman color (available: red, green, blue, yellow, pink)")
+	rootCmd.Flags().Int32VarP(&speed, "speed", "s", 1, "Set pacman multiple speed (default: 1)")
 }
 
 func getSize() (int, int) {
@@ -76,6 +83,31 @@ func getSize() (int, int) {
 	var rows, cols int
 	fmt.Sscan(string(out), &rows, &cols)
 	return rows, cols
+}
+
+func style(color string, lines []string) []string {
+	var styled []string
+	var style lipgloss.Style
+
+	switch color {
+	case "red":
+		style = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF0000"))
+	case "green":
+		style = lipgloss.NewStyle().Foreground(lipgloss.Color("#00FF00"))
+	case "blue":
+		style = lipgloss.NewStyle().Foreground(lipgloss.Color("#0000FF"))
+	case "yellow":
+		style = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFF00"))
+	case "pink":
+		style = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFC0CB"))
+	default:
+		style = lipgloss.NewStyle()
+	}
+
+	for _, line := range lines {
+		styled = append(styled, style.Render(line))
+	}
+	return styled
 }
 
 var pac = []string{
